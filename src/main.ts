82c20ef09a654d1d22753372ef3bd5e244c9f857
devopscomputer <pauloti.engenharia@gmail.com>
2026-01-24 01:19:15 -0300

    chore: update src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { json, urlencoded } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
+  app.use(json({ limit: '10mb' }));
+  app.use(urlencoded({ extended: true, limit: '10mb' }));
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
 }
+
 bootstrap();
