89e4ea52a2ad46660f0a27d758723367b0a6235e
devopscomputer <pauloti.engenharia@gmail.com>
2026-01-24 01:19:18 -0300

    chore: update src/storage/

---
+++ src/storage/storage.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { UploadService } from './upload.service';
+import { UploadController } from './upload/upload.controller';
+
+@Module({
+  providers: [UploadService],
+  controllers: [UploadController],
+  exports: [UploadService], // IMPORTANTE: Para o ProcessorService conseguir usar
+})
+export class StorageModule {}
