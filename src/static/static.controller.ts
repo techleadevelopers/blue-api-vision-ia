121272919bccf5ffec943b9c04229d635529add1
devopscomputer <pauloti.engenharia@gmail.com>
2026-01-31 14:39:16 -0300

    create: update src/static/static.controller.ts

--- src/static/static.controller.ts
+++ src/static/static.controller.ts
@@ -4,22 +4,21 @@ import { join } from 'path';
 
 @Controller()
 export class StaticController {
+  private readonly verificationText =
+    'tiktok-developers-site-verification=lyuuCaAPpEkg7jvhNZ0m2SZkLJRG7UVW';
+
   // Rota explícita para verificação do TikTok no caminho raiz
   @Get('tiktok9DJJtMAHBJYGN75iA8AnT163uOLbWa7f.txt')
   getTikTokVerification(@Res() res: Response) {
     res.type('text/plain');
-    return res.send(
-      'tiktok-developers-site-verification=9DJJtMAHBJYGN75iA8AnT163uOLbWa7f',
-    );
+    return res.send(this.verificationText);
   }
 
   // Suporta barra final que alguns validadores adicionam
   @Get('tiktok9DJJtMAHBJYGN75iA8AnT163uOLbWa7f.txt/')
   getTikTokVerificationSlash(@Res() res: Response) {
     res.type('text/plain');
-    return res.send(
-      'tiktok-developers-site-verification=9DJJtMAHBJYGN75iA8AnT163uOLbWa7f',
-    );
+    return res.send(this.verificationText);
   }
 
   @Get('terms')
