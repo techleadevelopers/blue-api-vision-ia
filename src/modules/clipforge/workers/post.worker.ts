cdcea0f94bf315b538a9e940688fb3df084b0404
devopscomputer <pauloti.engenharia@gmail.com>
2026-01-31 00:09:09 -0300

    create: update src/modules/clipforge/workers/post.worker.ts

--- src/modules/clipforge/workers/post.worker.ts
+++ src/modules/clipforge/workers/post.worker.ts
@@ -1,16 +1,19 @@
-import { Process, Processor } from '@nestjs/bullmq';
+import { Processor, WorkerHost } from '@nestjs/bullmq';
 import { Job } from 'bullmq';
 import { ClipforgeService } from '../clipforge.service';
 import { CLIPFORGE_QUEUES } from '../queues/clipforge.queues';
 import { JobStatus } from '../domain/job/job.model';
 import { ENV } from '../../../config/env.config';
 
-@Processor(CLIPFORGE_QUEUES.POST_QUEUE)
-export class PostWorker {
-  constructor(private readonly clipforge: ClipforgeService) {}
+@Processor(CLIPFORGE_QUEUES.POST_QUEUE, {
+  concurrency: ENV.CLIPFORGE_CONCURRENCY.POST,
+})
+export class PostWorker extends WorkerHost {
+  constructor(private readonly clipforge: ClipforgeService) {
+    super();
+  }
 
-  @Process({ name: 'post.queue', concurrency: ENV.CLIPFORGE_CONCURRENCY.POST })
-  async handle(job: Job<{ jobId: string; payload: Record<string, unknown> }>) {
+  async process(job: Job<{ jobId: string; payload: Record<string, unknown> }>) {
     try {
       // TODO: integrar SDK das plataformas (TikTok/IG/YouTube) dependendo do account
       await this.clipforge.markStatus(job.data.jobId, JobStatus.PUBLISHED, 'post.published');
